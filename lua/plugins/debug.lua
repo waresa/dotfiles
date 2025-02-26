17d9d36f0d0fa1b5145c161a548da605334fbe9d
Wares Amiry <67963850+waresa@users.noreply.github.com>
2025-02-26 13:17:21 +0100

    updated the ui to close on disconnect

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -3,17 +3,74 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
     },
     config = function()
       local dap = require("dap")
-
       -- PHP Debug configuration
       dap.adapters.php = {
         type = "executable",
         command = "node",
         args = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
       }
+
+      -- Custom highlighting
+      vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+      vim.api.nvim_set_hl(0, "DapBreakpointRejected", { ctermbg = 0, fg = "#d8b113", bg = "#31353f" })
+      vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+      vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+      -- Signs
+      vim.fn.sign_define(
+        "DapBreakpoint",
+        { text = "⬤", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+      )
+      vim.fn.sign_define(
+        "DapBreakpointCondition",
+        { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+      )
+      vim.fn.sign_define(
+        "DapBreakpointRejected",
+        {
+          text = "",
+          texthl = "DapBreakpointRejected",
+          linehl = "DapBreakpointRejected",
+          numhl = "DapBreakpointRejected",
+        }
+      )
+      vim.fn.sign_define(
+        "DapLogPoint",
+        { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+      )
+      vim.fn.sign_define(
+        "DapStopped",
+        { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+      )
+
+      -- Add a custom close command that ensures UI is closed
+      vim.api.nvim_create_user_command("DapUIClose", function()
+        require("dapui").close()
+        -- Force close any remaining windows
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+          if ft:match("^dap") or ft:match("^dapui") then
+            vim.api.nvim_win_close(win, true)
+          end
+        end
+      end, {})
     end,
+    keys = {
+      -- Override the default close command to use our custom close
+      {
+        "<leader>dx",
+        function()
+          require("dap").terminate()
+          vim.cmd("DapUIClose")
+        end,
+        desc = "Terminate & Close Debug UI",
+      },
+    },
   },
   {
     "rcarriga/nvim-dap-ui",
@@ -21,20 +78,54 @@ return {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      require("dapui").setup()
+      local dapui = require("dapui")
+
+      dapui.setup({
+        layouts = {
+          {
+            elements = {
+              { id = "scopes", size = 0.25 },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks", size = 0.25 },
+              { id = "watches", size = 0.25 },
+            },
+            size = 40,
+            position = "bottom", -- Show UI at the bottom instead of left
+          },
+          {
+            elements = {
+              { id = "repl", size = 0.5 },
+              { id = "console", size = 0.5 },
+            },
+            size = 10,
+            position = "bottom",
+          },
+        },
+        force_buffers = true, -- Ensure buffers are cleared when UI is closed
+      })
 
       -- Automatically open DAP UI when debugging starts
-      local dap, dapui = require("dap"), require("dapui")
+      local dap = require("dap")
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
       end
       dap.listeners.before.event_terminated["dapui_config"] = function()
         dapui.close()
       end
-      dap.listeners.before.event_exited["dapui_config"] = function()
+      dap.listeners.after.disconnect["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.after.event_exited["dapui_config"] = function()
         dapui.close()
       end
     end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = true,
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    },
   },
   {
     "williamboman/mason.nvim",
